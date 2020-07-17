d891d35c67e45baf517204df98b2b9edb7fc8a81
D P Mahesh <mahesh@chatakatech.com>
2020-07-17 15:50:21 +0530

    master

---
+++ app.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+st.title("Welcome to the Uber NYC Taxi fare Demo App")
+
+st.sidebar.header("User Input Parameters")
+x=st.sidebar.slider("User Input Data")
+y=st.sidebar.date_input("When is your birthday")
+st.write(x)
+st.write(y)
+
+chart_data = pd.DataFrame(
+     np.random.randn(50, 3),
+     columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
