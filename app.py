8049250c088850d5803b9aa6990a5595fef8bbcd
D P Mahesh <mahesh@chatakatech.com>
2020-07-17 16:11:23 +0530

    added display write statement

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ st.sidebar.header("User Input Parameters")
 x=st.sidebar.slider("User Input Data")
 y=st.sidebar.date_input("When is your birthday")
 z=st.sidebar.text_input('Enter First Name:')
+
 st.write(x)
 st.write(y)
+st.write(z)
 
 chart_data = pd.DataFrame(
      np.random.randn(50, 3),
