d40ddfaa06b58e7af0373d02dc89340fce2de230
dpmahesh <mahesh@chatakatech.com>
2023-05-08 13:42:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,17 +10,20 @@ x=st.sidebar.slider("User Input Data")
 y=st.sidebar.date_input("When is your birthday")
 z=st.sidebar.text_input('Enter First Name:')
 
-st.write("The dat you entered by moving the slider on the left:")
-st.header(x)
+st.header("The data you entered by moving the slider on the left:")
+st.write(x)
 
-st.write("Your Date of Birth")
-st.header(y)
-st.write("The text you enetered in the input field in the side bar")
-st.header(z)
+st.header("Your Date of Birth")
+st.write(y)
+
+st.header("The text you enetered in the input field in the side bar")
+st.write(z)
+
 
 chart_data = pd.DataFrame(
      np.random.randn(50, 3),
      columns=["a", "b", "c"])
 
+st.header("Some sample charts")
 st.bar_chart(chart_data)
 st.line_chart(chart_data)
